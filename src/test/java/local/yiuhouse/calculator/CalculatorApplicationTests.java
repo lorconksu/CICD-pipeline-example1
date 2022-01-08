98084a2de3e20d6a02c93c10bebda6cccc6b675f
William Yiu <wyiu@yiuhouse.local>
2022-01-08 10:15:24 -0500

    Added springboot code

---
+++ src/test/java/local/yiuhouse/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package local.yiuhouse.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
